50e8ed3aac713a620632cf39e586aacd950f957d
Vladislav Chudaev <vladislav.chudaev1998>
2024-10-20 14:37:35 +0300

    123

--- SimpleArrays/Program.cs
+++ SimpleArrays/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+Console.WriteLine("*****Fun with arrays*****");
+SimpleArrays();
+
+Console.ReadLine();
+
+static void SimpleArrays()
+{
+    Console.WriteLine("=> Simple Array Creation.");
+
+    int[] myInts = new int[3];
+    myInts[0] = 100;
+    myInts[1] = 200;
+    myInts[2] = 300;
+
+    foreach (int i in myInts)
+    {
+        Console.WriteLine(i);
+    }
+    Console.WriteLine();
+
+    string[] booksOnDotNet = new string[100];
+    Console.WriteLine();
+}
+
