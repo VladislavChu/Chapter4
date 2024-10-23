80b52ba90283b2e96c4bed3a474bbcc9391d1df6
Vladislav Chudaev <vladislav.chudaev1998>
2024-10-23 09:10:40 +0300

    array initialization

---
+++ ArrayInitialization/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+ArrayInitialization();
+
+
+static void ArrayInitialization()
+{
+    Console.WriteLine("=> Array Initialization.");
+
+
+    string[] stringArray = new string[] { "one", "two", "three" };
+    Console.WriteLine($"stringArray has {stringArray.Length} elements.");
+
+    bool[] boolArray = { false, false, true };
+    Console.WriteLine($"boolArray has {boolArray.Length} elements.");
+
+    int[] intArray = new int[4] { 1, 2, 3, 0 };
+    Console.WriteLine($"intArray has {intArray.Length} elements.");
+    Console.WriteLine();
+}
