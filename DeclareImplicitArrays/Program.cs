4cb66b55d7e3152db4744c5d169f520e1a875e38
Vladislav Chudaev <vladislav.chudaev1998>
2024-10-24 09:40:05 +0300

    Implicit Array Initialization

---
+++ DeclareImplicitArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+DeclareImplicitArrays();
+
+
+static void DeclareImplicitArrays()
+{
+    Console.WriteLine("=> Implicit Array Initialization.");
+
+    var a = new[] { 1, 10, 100, 1000 };
+    Console.WriteLine($"a is a: {a}");
+
+    var b = new[] { 1, 1.5, 2, 2.5 };
+    Console.WriteLine($"b is a: {b.ToString()}");
+
+    var c = new[] { "hello", null, "world" };
+    Console.WriteLine($"c is a: {c.ToString()}");
+    Console.WriteLine();
+}
