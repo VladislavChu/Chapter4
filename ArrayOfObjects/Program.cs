6a9eed1ba0bb60ab686b42a579a528d2f914f632
Vladislav Chudaev <vladislav.chudaev1998>
2024-10-25 09:35:49 +0300

    ArrayOfObjects

---
+++ ArrayOfObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+ArrayOfObjects();
+static void ArrayOfObjects()
+{
+    Console.WriteLine("=> Array of objects.");
+
+    object[] myObjects = new object[4];
+    myObjects[0] = 10;
+    myObjects[1] = false;
+    myObjects[2] = new DateTime(1969, 3, 24);
+    myObjects[3] = "Form & Void.";
+
+    foreach (object obj in myObjects)
+    {
+        Console.WriteLine($"Type: {obj.GetType()}, Value: {obj}");
+    }
+    Console.WriteLine();
+}
