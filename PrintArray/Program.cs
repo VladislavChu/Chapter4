967d8dba3f965d76b6dd55c72c44295053a7a85c
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-03 20:13:25 +0300

    Print Arrays

---
+++ PrintArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+PassAndReceiveArrays();
+
+
+static void PrintArray(int[] myInts)
+{
+    for (int i = 0; i < myInts.Length; i++)
+    {
+        Console.WriteLine($"Item {i} is {myInts[i]}");
+    }
+}
+
+static string[] GetStringArray()
+{
+    string[] theStrings = {"Hello", "from", "GetStringArray"};
+    return theStrings;
+}
+
+
+static void PassAndReceiveArrays()
+{
+    Console.WriteLine("=> Arrays as params and return values.");
+
+    int[] ages = { 20, 22, 23, 0 };
+    PrintArray(ages);
+
+    string[] strs = GetStringArray();
+    foreach(string s in strs) 
+        Console.WriteLine(s);
+    Console.WriteLine();
+}
