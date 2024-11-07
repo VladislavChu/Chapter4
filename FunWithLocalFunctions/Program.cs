400f6abaf2357caa1e8c8e0e7bd84dece7ee7129
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-07 09:25:45 +0300

    Local Functions

---
+++ FunWithLocalFunctions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+static int Add(int x, int y)
+{
+    //здесь должна выполняться проверка достоверности
+    return x + y;
+}
+
+static int AddWrapper(int x, int y)
+{
+    //здесь должна выполняться проверка достоверности
+    return Add();
+    int Add()
+    {
+        return x + y; 
+    }
+}
--- SystemArrayFunctionality/Program.cs
+++ SystemArrayFunctionality/Program.cs
@@ -2,6 +2,7 @@
 
 
 SystemArrayFunctionality();
+IndexAndRangesFunctionality();
 
 
 
@@ -40,4 +41,52 @@ static void SystemArrayFunctionality()
     Console.WriteLine("\n");
 
 
+}
+
+
+static void IndexAndRangesFunctionality()
+{
+    Console.WriteLine("=> Working with Indices and Ranges.");
+    // Initialize items at startup.
+    string[] gothicBands = { "Tones on Tail", "Bauhaus", "Sisters of Mercy" };
+
+    // ... and print them.
+    for (int i = 0; i < gothicBands.Length; i++)
+    {
+        Index idx = i;
+        // Print a name
+        Console.Write(gothicBands[idx] + ", ");
+    }
+    Console.WriteLine("\n");
+    for (int i = 1; i <= gothicBands.Length; i++)
+    {
+        Index idx = ^i;
+        // Print a name
+        Console.Write(gothicBands[idx] + ", ");
+    }
+    Console.WriteLine("\n");
+
+    foreach (var itm in gothicBands[0..2])
+    {   // Print a name
+        Console.Write(itm + ", ");
+    }
+    Console.WriteLine("\n");
+    Range r = 0..2; //the end of the range is exclusive
+    foreach (var itm in gothicBands[r])
+    {   // Print a name
+        Console.Write(itm + ", ");
+    }
+    Console.WriteLine("\n");
+    Index idx1 = 0;
+    Index idx2 = 2;
+    r = idx1..idx2; //the end of the range is exclusive
+    foreach (var itm in gothicBands[r])
+    {   // Print a name
+        Console.Write(itm + ", ");
+    }
+    Console.WriteLine("\n");
+
+    Console.WriteLine($"Array length is {gothicBands[..].Length}");
+    Console.WriteLine($"Array length is {gothicBands[0..3].Length}");
+    Console.WriteLine($"Array length is {gothicBands[0..^0].Length}");
 }
