9e1a85240be11083496cc81c81295c7fb0a88d36
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-08 09:23:54 +0300

    fun with methods

---
+++ AddWrapperWithSideEffect/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+AddWrapperWithStatic(3, 4);
+
+
+
+static int AddWrapperWithStatic(int x, int y)
+    {
+    //здесь должна быть какая-то проверка
+    return Add(x, y);
+    static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
