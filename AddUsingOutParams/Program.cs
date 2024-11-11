01320134bec6e7d22b977017f7c3ee82a5285a5e
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-11 09:30:31 +0300

    AddUsingOutParams

---
+++ AddUsingOutParams/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+AddUsingOutParam(90, 90, out int ans);
+Console.WriteLine(ans);
+
+
+
+
+static void AddUsingOutParam(int x, int y, out int ans)
+{
+    ans = x + y;
+}
--- FunWithMethods/Program.cs
+++ FunWithMethods/Program.cs
@@ -1,4 +1,23 @@
 ﻿using System;
 
 
+int x = 10;
+int y = 20;
 
+Console.WriteLine($"before call: x = {x}, y = {y}");
+Console.WriteLine($"answer is: {Add(x, y)}");
+Console.WriteLine($"after call: x = {x}, y = {y}");
+
+
+
+
+
+static int Add(int x, int y)
+{
+    int ans = x + y;
+
+    x = 10000;
+    y = 88888;
+
+    return ans;
+}
