990887d4aba6ab334ae3883707bc5a76712233a9
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-12 08:41:04 +0300

    fill these values

---
+++ FillTheseValues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+static void FillTheseValues(out int a, out string b, out bool c)
+{
+    a = 9;
+    b = "Enjoy your string.";
+    c = true;
+}
+
+
+Console.WriteLine("***** Fun with methods *****");
+FillTheseValues(out int i, out string str, out bool b);
+Console.WriteLine("Int is: {0}", i); // Вывод целочисленного значения
+Console.WriteLine("String is: {0}", str); // Вывод строкового значения
+Console.WriteLine("Boolean is: {0}", b); // Вывод булевского значения
+Console.ReadLine();
