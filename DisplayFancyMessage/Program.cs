7e1e0ef82495c5dfcdb21e009a421f6128a63d52
Vladislav Chudaev <vladislav.chudaev1998@gmail.com>
2024-11-19 19:11:05 +0300

    Fancy Message

---
+++ DisplayFancyMessage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Authentication;
+
+Console.WriteLine("*****Fun with methods*****");
+DisplayFancyMessage(message: "Wow! Very Fancy indeed!", textColor: ConsoleColor.DarkRed, backgroundColor: ConsoleColor.White);
+
+DisplayFancyMessage(backgroundColor: ConsoleColor.Cyan, message: "Vladik is the best!!!!", textColor: ConsoleColor.Magenta);
+
+DisplayFancyMessage(textColor: ConsoleColor.Magenta, ConsoleColor.Cyan, "Vladik is the best!!!!");
+
+
+
+
+static void DisplayFancyMessage(ConsoleColor textColor, ConsoleColor backgroundColor, string message)
+{
+    //сохранить старые цвета для их восстановления после вывода сообщения
+    ConsoleColor oldTextColor = Console.ForegroundColor;
+    ConsoleColor oldBackgroundColor = Console.BackgroundColor;
+
+    // установить новые цвета и вывести сообщение
+    Console.ForegroundColor = textColor;
+    Console.BackgroundColor = backgroundColor;
+    Console.WriteLine(message);
+
+    //вернуть цвета обратно
+    Console.ForegroundColor = oldTextColor;
+    Console.BackgroundColor = oldBackgroundColor;
+
+    /*Console.ForegroundColor = textColor;
+    Console.BackgroundColor = backgroundColor;
+    Console.WriteLine(message);
+    */
+}
