5c14b92f3184c28aa9ebe9773d7c5dafeb231d48
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-23 21:13:10 +0300

    method overloading

---
+++ FunWithMethodsOverloading/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using FunWithMethodsOverloading;
+using static FunWithMethodsOverloading.AddOperations;
+
+Console.WriteLine("***** Fun with Method Overloading *****\n");
+//int
+Console.WriteLine(Add(10, 10));
+//long
+Console.WriteLine(Add(900_000_000_000, 900_000_000_000));
+//double
+Console.WriteLine(Add(4.3, 4.4));
