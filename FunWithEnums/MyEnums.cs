09f0e3fc2b99975b87e206a5495e5a273d8df675
Vladislav Chudaev <vladislav.chudaev1998>
2024-11-29 08:57:06 +0300

    12321412

---
+++ FunWithEnums/MyEnums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithEnums
+{
+
+        public enum EmpTypeEnum : byte
+        {
+            Manager,        //0
+            Grunt,          //1
+            Contractor,     //2
+            VicePresident   //3
+        }
+
+}
--- FunWithEnums/Program.cs
+++ FunWithEnums/Program.cs
@@ -1,52 +1,47 @@
 ﻿using System;
+using FunWithEnums;
+using static FunWithEnums.EmpTypeEnum;
 
 
 
 
-Console.WriteLine("***** Fun with Enums *****");
-//EmpTypeEnum emp = Enum.GetValues(typeof(EmpTypeEnum);
-//AskForBonus(emp);
-//Console.WriteLine($"EmpTypeEnum uses a {Enum.GetUnderlyingType(emp.GetType())} for storage");
 
-foreach (EmpTypeEnum i in EmpTypeEnum.GetValues(typeof(EmpTypeEnum)))
-{
-    AskForBonus(i);
-}
-Console.WriteLine($"EmpTypeEnum uses a {Enum.GetUnderlyingType(typeof(EmpTypeEnum))} for storage");
-Console.ReadLine();
+        Console.WriteLine("***** Fun with Enums *****");
+        EmpTypeEnum emp = EmpTypeEnum.Contractor;
+        Console.WriteLine($"emp is a {emp.ToString()}");
+        Console.WriteLine($"{emp.ToString()} = {(byte)emp}");
+        //AskForBonus(emp);
+        //Console.WriteLine($"EmpTypeEnum uses a {Enum.GetUnderlyingType(emp.GetType())} for storage");
 
-
-
-static void AskForBonus(EmpTypeEnum e)
-{
-    switch (e)
+        /*foreach (EmpTypeEnum i in EmpTypeEnum.GetValues(typeof(EmpTypeEnum)))
         {
-            case EmpTypeEnum.Manager:
-            Console.WriteLine("How about stock options instead?");
-            break;
-
-            case EmpTypeEnum.Grunt:
-            Console.WriteLine("You have got to be kidding...");
-            break;
-
-            case EmpTypeEnum.Contractor:
-            Console.WriteLine("You already get enough cash...");
-            break;
-
-            case EmpTypeEnum.VicePresident:
-            Console.WriteLine("VERY GOOD, Sir! ");
-            break;
-    }
-}
+            AskForBonus(i);
+        }*/
+        //Console.WriteLine($"EmpTypeEnum uses a {Enum.GetUnderlyingType(typeof(EmpTypeEnum))} for storage");
+        Console.ReadLine();
 
 
 
+        static void AskForBonus(EmpTypeEnum e)
+        {
+            switch (e)
+            {
+                case EmpTypeEnum.Manager:
+                    Console.WriteLine("How about stock options instead?");
+                    break;
+
+                case EmpTypeEnum.Grunt:
+                    Console.WriteLine("You have got to be kidding...");
+                    break;
+
+                case EmpTypeEnum.Contractor:
+                    Console.WriteLine("You already get enough cash...");
+                    break;
+
+                case EmpTypeEnum.VicePresident:
+                    Console.WriteLine("VERY GOOD, Sir! ");
+                    break;
+            }
+        }
 
 
-enum EmpTypeEnum : byte
-{
-    Manager,        //0
-    Grunt,          //1
-    Contractor,     //2
-    VicePresident   //3
-}
