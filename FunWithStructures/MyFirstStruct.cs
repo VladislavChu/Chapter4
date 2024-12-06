ef51be93c1dd3b44571c255865f52c4bc5846fbe
Vladislav Chudaev <vladislav.chudaev1998>
2024-12-06 09:47:11 +0300

    my first structure

---
+++ FunWithStructures/MyFirstStruct.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithStructures
+{
+    struct Point
+    {
+        public int X;
+        public int Y;
+
+
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+
+        public void Increment()
+        {
+            X++;
+            Y++;
+        }
+
+        public void Decrement()
+        {
+            X--;
+            Y--;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"x = {X}, y = {Y}");
+        }
+    }
+}
---
+++ FunWithStructures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FunWithStructures;
+using static FunWithStructures.Point;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** A First Look at Structures *****\n");
+
+        Point myPoint;
+
+        myPoint.X = 349;
+        myPoint.Y = 76;
+        myPoint.Display();
+
+        myPoint.Decrement();
+        myPoint.Display();
+       
+
+        Point p1 = new Point();
+        //p1.X = 10;
+        p1.Display();
+    }
+}
