b33c2aeab54252f787d20bbcb0cd73b180e0dece
Vladislav Chudaev <vladislav.chudaev1998>
2024-12-16 08:11:21 +0300

    huita ne ponyal

---
+++ FunWithNullableReferenceTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+string? nullableString = null;
+TestClass? myNullableTestClass = null;
+TestClass myNonNullableClass = myNullableTestClass;
+#nullable disable
+TestClass anotherNullableClass = null;
+TestClass? badDefinition = null;
+string? anotherNullableString = null;
+#nullable restore
+
+Console.WriteLine("hello");
+
+
+public class TestClass
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+
