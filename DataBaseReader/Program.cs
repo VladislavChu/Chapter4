f76a2711b97c4947cbef7052410ca30a73c9cd17
Vladislav Chudaev <vladislav.chudaev1998>
2024-12-17 08:30:25 +0300

    Fun with null ? ??

--- DataBaseReader/Program.cs
+++ DataBaseReader/Program.cs
@@ -4,9 +4,44 @@ using static DataBaseReader.DBReader;
 
 
 
+
+
+
+
 Console.WriteLine("***** Fun with Nullable Value Types *****\n");
+TesterMethod(["dwad", " "]);
+Console.ReadLine();
 
 DBReader dr = new DBReader();
+
+//операция объединения с null
+int myData = dr.GetIntFromDatabase() ?? 100;
+Console.WriteLine($"Value of myData is: {myData}");
+Console.ReadLine();
+
+//операция присваивания с объединением с null
+int? nullableInt = null;
+nullableInt ??= 12;
+nullableInt ??= 14;
+Console.WriteLine(nullableInt);
+Console.ReadLine();
+
+//null- условная операция
+/* 
+ * static void TesterMethod(string[] args)
+{
+    if (args != null)
+    {
+        Console.WriteLine($"You sent me {args.Length} arguments.");
+    }
+}
+*/
+static void TesterMethod(string[] args)
+{
+    Console.WriteLine($"You sent me {args?.Length ?? 0} arguments.");
+}
+
+
 
 int? i = dr.GetIntFromDatabase();
 if (i.HasValue)
